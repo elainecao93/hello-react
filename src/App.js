fe51834ea69ca97296c51b2f15868f2dc22f76e5
elainecao93 <elainecao93@users.noreply.github.com>
2020-07-03 11:15:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ""};
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.value);
+    this.setState({value: ""});
+  }
+
+  render() {
+    return <form>
+        <input type="textarea" value={this.state.value} id="post" name="post" onChange={this.onChange}></input>
+        <br></br>
+        <input type="submit" value="Submit" onClick={this.onSubmit}/>
+        Length: {this.state.value.length}
+    </form>
+  }
+}
+
+class History extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  onDelete(id) {
+    this.props.deletePost(id);
+  }
+
+  render() {
+    const items = this.props.posts.map((post, index) =>
+      <Post post={post} id={index} onDelete = {this.onDelete}/>
+    );
+    return <ul>{items}</ul>;
+  }
+}
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {post: props.post, id: props.id};
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  onDelete() { 
+    this.props.onDelete(this.props.id)
+  }
+
+  render() {
+    return <li>
+      {this.state.post}
+      <input type="submit" value="Delete" onClick = {this.onDelete}/>
+    </li>
+  }
+}
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.state = {posts: []};
+  }
+
+  onSubmit(value) {
+    const p = this.state.posts;
+    this.setState({posts: p.slice().concat([value])});
+    alert(value);
+    alert(this.state.posts.length)
+  }
+
+  deletePost(id) {
+    alert("delete button clicked for " + id);
+    const p = this.state.posts;
+    this.setState({posts: p.slice().splice(id, 1)})
+  }
+
+  render() {
+    return (
+      <div>
+        <Entry onSubmit = {this.onSubmit}/>
+        <History posts = {this.state.posts} deletePost = {this.deletePost}/>
+      </div>
+    );
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Blog/>
     </div>
   );
 }
